972fac7561aa3523bfac46722699443645920737
lunaw <87834094+lunawe@users.noreply.github.com>
2021-08-26 10:59:02 -0400

    Added splash.py

---
+++ splash.py
@@ -0,0 +1,38 @@
+import os
+
+def clear():
+    if(os.name == "nt"):
+        os.system("cls")
+    else:
+        os.system("clear")
+
+clear()
+
+rows,cols = os.popen('stty size', 'r').read().split()
+logo_height = 3
+sp = " "
+try:
+    x = int(int(rows) / 2)
+except:
+    x = int(rows) / 2
+
+splash1 = "█ █ █▄ █ █▀▄ █▀▀ █▀█ ▀█▀ ▄▀█ █   █▀▀"
+splash2 = "█▄█ █ ▀█ █▄▀ ██▄ █▀▄  █  █▀█ █▄▄ ██▄"
+splash3 = "          Terminal Edition"
+
+logo_len = len(splash1)
+y = int((int(cols) - logo_len) / 2)
+
+splash = [
+    splash1,
+    splash2,
+    splash3
+]
+
+print("\n"*x)
+for i in splash:
+    print(" "*y,i)
+print("\n"*(x-1))
+logo_len = len("Press Enter...")
+y = int((int(cols) - logo_len) / 2)
+input(f"{sp*y}Press Enter...")
